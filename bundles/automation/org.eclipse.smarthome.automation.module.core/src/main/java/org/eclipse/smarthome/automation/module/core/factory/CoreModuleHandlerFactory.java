82b6200a0270aa3645b9ba680bfd313d17ca38cb
Benedikt Niehues <benedikt.niehues@itemis.de>
2017-02-02 14:31:01 +0100

    Added RunRuleAction for AutomationComponent
    Signed-off-by: Benedikt Niehues <benedikt.niehues@itemis.de>

--- bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/factory/CoreModuleHandlerFactory.java
+++ bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/factory/CoreModuleHandlerFactory.java
@@ -25,6 +25,7 @@ import org.eclipse.smarthome.automation.module.core.handler.ItemCommandTriggerHa
 import org.eclipse.smarthome.automation.module.core.handler.ItemStateConditionHandler;
 import org.eclipse.smarthome.automation.module.core.handler.ItemStateTriggerHandler;
 import org.eclipse.smarthome.automation.module.core.handler.RuleEnableHandler;
+import org.eclipse.smarthome.automation.module.core.handler.RunRuleHandler;
 import org.eclipse.smarthome.core.events.EventPublisher;
 import org.eclipse.smarthome.core.items.ItemRegistry;
 import org.osgi.service.component.ComponentContext;
@@ -48,7 +49,7 @@ public class CoreModuleHandlerFactory extends BaseModuleHandlerFactory {
                     ItemStateTriggerHandler.CHANGE_MODULE_TYPE_ID, ItemStateConditionHandler.ITEM_STATE_CONDITION,
                     ItemCommandActionHandler.ITEM_COMMAND_ACTION, GenericEventTriggerHandler.MODULE_TYPE_ID,
                     GenericEventConditionHandler.MODULETYPE_ID, GenericEventConditionHandler.MODULETYPE_ID,
-                    CompareConditionHandler.MODULE_TYPE, RuleEnableHandler.UID });
+                    CompareConditionHandler.MODULE_TYPE, RuleEnableHandler.UID, RunRuleHandler.UID });
 
     private ItemRegistry itemRegistry;
     private EventPublisher eventPublisher;
@@ -182,6 +183,8 @@ public class CoreModuleHandlerFactory extends BaseModuleHandlerFactory {
                 return postCommandActionHandler;
             } else if (RuleEnableHandler.UID.equals(moduleTypeUID)) {
                 return new RuleEnableHandler((Action) module, ruleRegistry);
+            } else if (RunRuleHandler.UID.equals(moduleTypeUID)){
+            	return new RunRuleHandler((Action)module, ruleRegistry);
             }
         }
 
